c0d71f70b2bdd55ed4967fd53f99c5f1392d34a1
arul121 <55161139+arul121@users.noreply.github.com>
2021-08-22 16:08:21 +0700

    v3

--- assets/js/get.js
+++ assets/js/get.js
@@ -1,13 +1,24 @@
 function bisa() {
-    fetch("https://api.apispreadsheets.com/data/15883/").then(res=>{
-	if (res.status === 200){
-		// SUCCESS
-		res.json().then(data=>{
-			const yourData = data
-		}).catch(err => console.log(err))
-	}
-	else{
-		// ERROR
-	}
-})
+                                    fetch("https://api.apispreadsheets.com/data/15916/").then(res=>{
+                                    if (res.status === 200){
+                                        // SUCCESS
+                                        res.json().then(data=>{
+                                            var data1= Object.values(data);
+                                            var yourData= Object.values(data1[0]);
+                                            let textox="";
+                                        for(let i in yourData){
+                                            var doto = Object.values(yourData[i]); //disini kalo mau ganti data
+                                            textnama = doto[0]; //jenis data
+                                            textwish = doto[5]; //jenis data
+                                            textox += "<div><strong>"+textnama+"</strong><p>"+textwish+"</p><hr></div>";
+                                            console.log(i);
+                                            document.getElementById("keren").innerHTML = textox;
+                                        }
+                                        
+                                        }).catch(err => console.log(err))
+                                    }
+                                    else{
+                                        // ERROR
+                                    }
+                                })
 }
--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,10 +1,11 @@
    function bang (){
 			$.ajax({
-				url:"https://api.apispreadsheets.com/data/15883/",
+				url:"https://api.apispreadsheets.com/data/15916/",
 				type:"post",
 				data:$("#myForm").serializeArray(),
 				success: function(){
 					location.reload();
+          $('#myForm')[0].reset();
 				},
 				error: function(){
 					alert("There was an error :(")
